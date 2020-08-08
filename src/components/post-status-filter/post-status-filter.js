31de309149df2e59455375983200f32e6e0b79e7
Roma <keepcalmkid33@gmail.com>
2020-08-08 17:36:03 +0300

    first commit. Structure was done and simple styles added.

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './post-status-filter';
+
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button>Все</button>
+            <button>Понравилось</button>
+        </div>
+    )
+}
+
+export default PostStatusFilter;
