57a5bd52dde2b1a952ad12a2aac476513f5bd695
Roma <keepcalmkid33@gmail.com>
2020-08-10 21:44:40 +0300

    added filter and search panel

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -19,11 +19,11 @@ const Header = styled.div`
 	}
 `
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
 	return (
 		<Header colored as='a'>
 			<h1>Roman Horbenko</h1>
-			<h2>5 записей, из них понравилось 0</h2>
+			<h2>{allPosts} записей, из них понравилось {liked}</h2>
 		</Header>
 	)
 }
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,13 +3,17 @@ import PostListItem from '../post-list-item';
 import {ListGroup} from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps}/>
+                <PostListItem
+                 {...itemProps}
+                 onDelete={() => onDelete(id)}
+                 onToggleImportant={() => onToggleImportant(id)}
+                 onToggleLiked={() => onToggleLiked(id)}/>
             </li>
         )
     });
